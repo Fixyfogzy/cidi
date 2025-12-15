ff4ab27b467b60fa1cd42c7a068f040f7da31753
Dally Mofiyinfoluwa <dallymofiyinfoluwa@gmail.com>
2025-12-15 07:35:54 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,29 +53,7 @@ document.querySelectorAll('.donate-dropdown').forEach(dropdown => {
 
 
 
-// Counter Animation for Impact Numbers
-function animateCounter() {
-    const counters = document.querySelectorAll('.impact-number');
-    const speed = 200; // The lower the slower
-    
-    counters.forEach(counter => {
-        const updateCount = () => {
-            const target = +counter.getAttribute('data-target');
-            const count = +counter.innerText;
-            
-            const inc = target / speed;
-            
-            if (count < target) {
-                counter.innerText = Math.ceil(count + inc);
-                setTimeout(updateCount, 1);
-            } else {
-                counter.innerText = target.toLocaleString();
-            }
-        };
-        
-        updateCount();
-    });
-}
+
 
 // Intersection Observer to trigger counter when section is in view
 const impactSection = document.querySelector('.impact-section');
